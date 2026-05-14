2f9c032e87c5208e5212d6f25ffb51eb87beedd5
JabirSiddiqueTalim <jsiddiquetalim@gmail.com>
2026-05-15 01:52:41 +0600

    GET response status

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
-import express from "express";
-const app = express();
+import express, { type Application, type Request, type Response } from "express";
+const app:Application = express();
 const port = 5000
 
-app.get('/user', (req, res) => {
-  res.send('Hello World I Am User!')
+app.get('/user', (req:Request, res:Response) => {
+  // res.send('Hello World I Am User!')
+  res.status(200).json({
+    message:"Express Level",
+    "author":"Next level"
+  })
 })
 
 app.listen(port, () => {
