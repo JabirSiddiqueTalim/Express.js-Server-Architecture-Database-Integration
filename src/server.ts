8ad6421e4f2b0e3d03caa402b103bae14ea543f8
JabirSiddiqueTalim <jsiddiquetalim@gmail.com>
2026-05-15 02:03:04 +0600

    Understanding the express Request and Response

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,23 @@
 import express, { type Application, type Request, type Response } from "express";
+import { json } from "node:stream/consumers";
 const app:Application = express();
 const port = 5000
 
-app.get('/user', (req:Request, res:Response) => {
+app.get('/', (req:Request, res:Response) => {
   // res.send('Hello World I Am User!')
   res.status(200).json({
     message:"Express Level",
     "author":"Next level"
   })
 })
+app.use(express.json())
+app.post('/',async(req:Request,res:Response)=>
+{
+  console.log(req.body);
+
+}
+)
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening On port ${port}`)
 })
