8fcf7e8165783c297f092aebcc9d2d88be4492ac
JabirSiddiqueTalim <jsiddiquetalim@gmail.com>
2026-05-15 03:28:29 +0600

    Setting Up Postgres with Neon Serverless Cloud

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,35 @@
 import express, { type Application, type Request, type Response } from "express";
 import { json } from "node:stream/consumers";
-const app:Application = express();
+import { Pool } from "pg";
+const app: Application = express();
 const port = 5000
 
-app.get('/', (req:Request, res:Response) => {
+const pool = new Pool({
+  connectionString: "postgresql://neondb_owner:npg_VYZEFvq7Np8A@ep-withered-smoke-aqgywwx4.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require"
+})
+
+app.get('/', (req: Request, res: Response) => {
   // res.send('Hello World I Am User!')
   res.status(200).json({
-    message:"Express Level",
-    "author":"Next level"
+    message: "Express Level",
+    "author": "Next level"
   })
 })
+
 app.use(express.json())
 app.use(express.text())
-app.use(express.urlencoded({extended:true}))
-app.post('/',async(req:Request,res:Response)=>
-{
+app.use(express.urlencoded({ extended: true }))
+
+app.post('/', async (req: Request, res: Response) => {
   // console.log(req.body);
-  const body=req.body;
-  
+  const body = req.body;
+
   res.status(202).json(
     {
-      message:"Data post finlay",
-      data:body,
+      message: "Data post finlay",
+      data: body,
     }
   )
-
 }
 )
 
