3794c8058c9b58d1adf054f0125f54499e93bd27
JabirSiddiqueTalim <jsiddiquetalim@gmail.com>
2026-05-17 18:35:18 +0600

    Implementing Modular pattern

--- src/modules/users/user.route.ts
+++ src/modules/users/user.route.ts
@@ -1,33 +1,8 @@
 import { Router, type Request, type Response } from "express";
 import { pool } from "../../db";
+import { userController } from "./user.controller";
 
 
 const router=Router();
-router.post('/', async (req: Request, res: Response) => {
-  // console.log(req.body);
-  const { name, email, password, age } = req.body;
-  try {
-    const result = await pool.query(`
-      INSERT INTO users(name ,email,password,age) VALUES($1,$2,$3,$4)
-      RETURNING *
-      `,
-      [name, email, password, age],
-    );
-    console.log(result)
-    res.status(202).json(
-      {
-        message: "Data post successfully",
-        data: result.rows[0]
-      }
-    )
-  } catch (error: any) {
-    res.status(500).json(
-      {
-        message: error.message,
-        error: error
-      }
-    )
-  }
-}
-)
+router.post('/', userController.createUser)
 export const userRouter=router;
